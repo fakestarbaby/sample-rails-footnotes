9bf55ae8103219a1acd0ccbbb39d0c3c537b3238
fakestarbaby <fakestarbaby@gmail.com>
2013-10-08 22:46:42 +0900

    Fixed haml & slim files not counted in Partials note

--- config/initializers/rails_footnotes.rb
+++ config/initializers/rails_footnotes.rb
@@ -1,5 +1,15 @@
 if defined?(Footnotes) && Rails.env.development?
-  Footnotes.run! # first of all
+  Footnotes.run!
 
-  # ... other init code
+  # Refe: https://github.com/josevalim/rails-footnotes/issues/83
+  Footnotes::Notes::LogNote::LoggingExtensions.module_eval do
+    def add(*args, &block)
+      super
+      logged_message = args[2] + "\n"
+      Footnotes::Notes::LogNote.log(logged_message)
+      logged_message
+    end
+  end
+
+  Footnotes::Filter.prefix = 'subl://open?url=file://%s&line=%d&column=%d'
 end
